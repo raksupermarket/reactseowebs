51c5b16b6f84d18c03292a7fad0c34e3da8c74f4
raksupermarket <55552565+raksupermarket@users.noreply.github.com>
2021-02-15 14:55:38 +0700

    Update SinglePage.js

--- src/SinglePage.js
+++ src/SinglePage.js
@@ -10,34 +10,34 @@ const SinglePage = ({ match }) => {
     <meta name="google-site-verification" content="YxQy4XuwrF6iIorC7vGlVbk8WLSFN-C9k8RSQ0ttoTs" />
         <title>{article.title}</title>
         <meta content="{article.desc}" name=" description" />
-        <meta content="{article.title}" property="og:title" />
-        <meta content="{article.desc}" property="og:description" />
-        <meta content="{article.img}" property="og:image" />
+        <meta content="Laravel POS aplikasi kasir toko grosir dan eceran web apps" property="og:title" />
+        <meta content="Mesin kasir online dengan penjualan grosir dan eceran include website web app laravelpos" property="og:description" />
+        <meta content="https://1.bp.blogspot.com/-8TDWnq41TuE/Xzt03g86CoI/AAAAAAAALN4/RRO9T2k3or8x4xx6vBDAmdn0lIHLU6oIACLcBGAsYHQ/s1500/aplikasi%2Btoko%2Bmesin%2Bkasir%2Bonline%2Blaravel%2Bpoint%2Bof%2Bsale%2Bcashier%2Bapps%2B%25282%2529.jpg" property="og:image" />
         <meta content="https://axcora.com/getaxcoracms" property="og:author" />
-        <meta content="{article.title}" name="facebook:title" />
+        <meta content="Laravel POS aplikasi kasir toko grosir dan eceran web apps" name="facebook:title" />
         <meta content="website" property="og:type" />
         <meta
           content="https://www.facebook.com/mesinkasircomplete"
           property="facebook:author"
         />
-        <meta content="{article.img}" name="facebook:image" />
-        <meta content="{article.title}" property="og:site_name" />
-        <meta content="{article.desc}" property="facebook:description" />
+        <meta content="https://1.bp.blogspot.com/-8TDWnq41TuE/Xzt03g86CoI/AAAAAAAALN4/RRO9T2k3or8x4xx6vBDAmdn0lIHLU6oIACLcBGAsYHQ/s1500/aplikasi%2Btoko%2Bmesin%2Bkasir%2Bonline%2Blaravel%2Bpoint%2Bof%2Bsale%2Bcashier%2Bapps%2B%25282%2529.jpg" name="facebook:image" />
+        <meta content="Laravel POS aplikasi kasir toko grosir dan eceran web apps" property="og:site_name" />
+        <meta content="Mesin kasir online dengan penjualan grosir dan eceran include website web app laravelpos" property="facebook:description" />
         <meta content="summary_large_image" name="twitter:card" />
         <meta
           content="https://twitter.com/hockeycomp"
           property="twitter:author"
         />
         <meta content="@hockeycomp" name="twitter:site" />
-        <meta content="{article.img}" name="twitter:image" />
-        <meta content="{article.desc}" name="twitter:description" />
-        <meta content="{article.title}" name="twitter:title" />
+        <meta content="https://1.bp.blogspot.com/-8TDWnq41TuE/Xzt03g86CoI/AAAAAAAALN4/RRO9T2k3or8x4xx6vBDAmdn0lIHLU6oIACLcBGAsYHQ/s1500/aplikasi%2Btoko%2Bmesin%2Bkasir%2Bonline%2Blaravel%2Bpoint%2Bof%2Bsale%2Bcashier%2Bapps%2B%25282%2529.jpg" name="twitter:image" />
+        <meta content="Mesin kasir online dengan penjualan grosir dan eceran include website web app laravelpos" name="twitter:description" />
+        <meta content="Laravel POS aplikasi kasir toko grosir dan eceran web apps" name="twitter:title" />
         <meta content="index, follow" name="robots" />
       </Helmet>
       <div className="row">
         <div className="col-12 col-md-12 p-3 p-md-5">
           <div className="shadow rounded p-3 p-md-5 uk-animation-slide-right">
-            <img className="img-fluid" src={article.img} />
+            <img className="img-fluid" src="https://1.bp.blogspot.com/-8TDWnq41TuE/Xzt03g86CoI/AAAAAAAALN4/RRO9T2k3or8x4xx6vBDAmdn0lIHLU6oIACLcBGAsYHQ/s1500/aplikasi%2Btoko%2Bmesin%2Bkasir%2Bonline%2Blaravel%2Bpoint%2Bof%2Bsale%2Bcashier%2Bapps%2B%25282%2529.jpg" />
             <h1>
               <strong>
                 <a href="/" className="text-danger">

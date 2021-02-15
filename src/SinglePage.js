59b2b2d3932eef2ffb2c71c1bd854add23c7bd35
raksupermarket <55552565+raksupermarket@users.noreply.github.com>
2021-02-15 18:35:01 +0700

    Update SinglePage.js

--- src/SinglePage.js
+++ src/SinglePage.js
@@ -12,7 +12,6 @@ const SinglePage = ({ match }) => {
         <meta content="{article.desc}" name=" description" />
         <meta content="Laravel POS aplikasi kasir toko grosir dan eceran web apps" property="og:title" />
         <meta content="Mesin kasir online dengan penjualan grosir dan eceran include website web app laravelpos" property="og:description" />
-        <meta content="https://1.bp.blogspot.com/-8TDWnq41TuE/Xzt03g86CoI/AAAAAAAALN4/RRO9T2k3or8x4xx6vBDAmdn0lIHLU6oIACLcBGAsYHQ/s1500/aplikasi%2Btoko%2Bmesin%2Bkasir%2Bonline%2Blaravel%2Bpoint%2Bof%2Bsale%2Bcashier%2Bapps%2B%25282%2529.jpg" property="og:image" />
         <meta content="https://axcora.com/getaxcoracms" property="og:author" />
         <meta content="Laravel POS aplikasi kasir toko grosir dan eceran web apps" name="facebook:title" />
         <meta content="website" property="og:type" />
@@ -20,7 +19,6 @@ const SinglePage = ({ match }) => {
           content="https://www.facebook.com/mesinkasircomplete"
           property="facebook:author"
         />
-        <meta content="https://1.bp.blogspot.com/-8TDWnq41TuE/Xzt03g86CoI/AAAAAAAALN4/RRO9T2k3or8x4xx6vBDAmdn0lIHLU6oIACLcBGAsYHQ/s1500/aplikasi%2Btoko%2Bmesin%2Bkasir%2Bonline%2Blaravel%2Bpoint%2Bof%2Bsale%2Bcashier%2Bapps%2B%25282%2529.jpg" name="facebook:image" />
         <meta content="Laravel POS aplikasi kasir toko grosir dan eceran web apps" property="og:site_name" />
         <meta content="Mesin kasir online dengan penjualan grosir dan eceran include website web app laravelpos" property="facebook:description" />
         <meta content="summary_large_image" name="twitter:card" />
@@ -29,7 +27,6 @@ const SinglePage = ({ match }) => {
           property="twitter:author"
         />
         <meta content="@hockeycomp" name="twitter:site" />
-        <meta content="https://1.bp.blogspot.com/-8TDWnq41TuE/Xzt03g86CoI/AAAAAAAALN4/RRO9T2k3or8x4xx6vBDAmdn0lIHLU6oIACLcBGAsYHQ/s1500/aplikasi%2Btoko%2Bmesin%2Bkasir%2Bonline%2Blaravel%2Bpoint%2Bof%2Bsale%2Bcashier%2Bapps%2B%25282%2529.jpg" name="twitter:image" />
         <meta content="Mesin kasir online dengan penjualan grosir dan eceran include website web app laravelpos" name="twitter:description" />
         <meta content="Laravel POS aplikasi kasir toko grosir dan eceran web apps" name="twitter:title" />
         <meta content="index, follow" name="robots" />
